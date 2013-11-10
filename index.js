b96e8826887c6c20fc9a4a05968599b4bb339dac
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2013-11-09 16:38:47 -0800

    refactor

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 require('./reqFrame')
 var emitter = require('events').EventEmitter
 
-navigator.getUserMedia = (navigator.getUserMedia || 
-                        navigator.webkitGetUserMedia ||
-                        navigator.mozGetUserMedia ||
-                        navigator.msGetUserMedia);
-
-if(!navigator.getUserMedia){
-    alert('no webcam or no getUserMedia support detected.  Try Using Chome')
-}
-
-module.exports = function(video, mirror){
+module.exports = function(stream, video, mirror, film){
 	
-	navigator.getUserMedia({audio: true, video: true}, function(stream){
-	    video.src = window.URL.createObjectURL(stream)
-	}, function(err){
-	    alert('no webcam or no getUserMedia support detected.  Try Using Chome')
-	})
 	
+	video.src = window.URL.createObjectURL(stream)
+    
 	var app = new emitter();
 	app.snapShot = snapShot;
 	app.record = record;
@@ -68,6 +56,7 @@ module.exports = function(video, mirror){
         
         var defaults = {
             shutterSpeed: 1000 / 24,
+            invert: false,
             filmSpeed: {
                 r: 5,
                 g: 5,
@@ -98,9 +87,10 @@ module.exports = function(video, mirror){
         }
         var d = 0;
     	var reflection = mirror.getContext('2d')
+    	var render = film.getContext('2d')
 
-    	var positive = reflection.getImageData(0,0,mirror.width, mirror.height)
-        positive = new Uint8ClampedArray(positive.data.length)
+    	var pos = render.getImageData(0,0,film.width, film.height)
+        var positive = new Uint8ClampedArray(pos.data.length)
 
     	for(var m = 0; m < mirror.width * mirror.height; m++){
     	    var index = m * 4;
@@ -128,10 +118,19 @@ module.exports = function(video, mirror){
         	var negative = reflection.getImageData(0,0,mirror.width,mirror.height);  
                 for(n=0; n<negative.width*negative.height; n++) {  
                     var index = n*4;   
-                    positive[index+0] =  positive[index+0] + (negative.data[index] / params.filmSpeed.r)
-                    positive[index+1] =  positive[index+1] + (negative.data[index+1] / params.filmSpeed.g)
-                    positive[index+2] =  positive[index+2] + (negative.data[index+2] / params.filmSpeed.b)  
+                    positive[index+0] =  vert(positive[index+0], (negative.data[index] / params.filmSpeed.r))
+                    positive[index+1] =  vert(positive[index+1], (negative.data[index+1] / params.filmSpeed.g))
+                    positive[index+2] =  vert(positive[index+2], (negative.data[index+2] / params.filmSpeed.b))
                 }
+    //        pos.data.set(positive)
+      //      render.putImageData(pos, 0, 0)
+        }
+        
+        function vert(a, b){
+            if(params.invert){
+                return a - b
+            }
+            else return a + b
         }
     }
     
