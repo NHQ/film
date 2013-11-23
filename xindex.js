fbcd6791fb181daf5918d3187462db7deeff9819
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2013-11-23 10:47:37 -0800

    crapped

--- xindex.js
+++ xindex.js
@@ -13,13 +13,28 @@ var getCSS = function(el, prop){
         return (parseInt(propValue) || 0)
 }
 
+var defaults = {
+    shutterSpeed: 1000/24,
+    invert: false,
+    blob: undefined,
+    filmSpeed: {
+        r: 1,
+        g: 1,
+        b: 1
+    },
+    r: 0,
+    g: 0,
+    b: 0,
+    a: 255
+};
+
 module.exports = function(video, mirror, exposure, render){
     
 	var reflection = mirror.getContext('2d')
     
 	var film = exposure.getContext('2d')
 	var print = render.getContext('2d')
-	
+	var params = setParams({})
 	
     var time = Time();
     var frameCount = 0;
@@ -27,26 +42,14 @@ module.exports = function(video, mirror, exposure, render){
     
 	var recording = false;
 	var frames = [];
-	var defaults = {
-        shutterSpeed: 2000,
-        invert: false,
-        filmSpeed: {
-            r: 20,
-            g: 11,
-            b: 10
-        },
-        r: 0,
-        g: 45,
-        b: 45,
-        a: 255
-    };
+
     
 	var app = new emitter();
 //    app.on('snapShot', snapShot)
 //    app.on('setParams', setParams)
     
     monitor()
-    setInterval(expose, 2111)
+    setInterval(expose, 1000/24)
     
     var last = 0
     
@@ -59,33 +62,32 @@ module.exports = function(video, mirror, exposure, render){
         window.frameRate = frameCount / (time.sinceBeginNS() / 1e9)
 	}
 	
-	function expose(params){
+	function setParams(p){
+	    if(!p) return;
+	    for (var attrname in defaults) {
+            if(typeof params.filmSpeed == 'number'){
+                var n = params.filmSpeed;
+                params.filmSpeed = {
+                    r: Math,ax(n, .001),
+                    g: Math,ax(n, .001),
+                    b: Math,ax(n, .001)
+                }
+            }
+            if(!params[attrname] && params[attrname] != 0) params[attrname] = defaults[attrname]
+        }
+        return params
+	}
+	
+	function expose(blob){
 	    
 	    var d = 0, fcp = 0;
-                        
-        if(params) {
-            for (var attrname in defaults) {
-                if(typeof params.filmSpeed == 'number'){
-                    var n = params.filmSpeed;
-                    params.filmSpeed = {
-                        r: n,
-                        g: n,
-                        b: n
-                    }
-                }
-                if(!params[attrname] && params[attrname] != 0) params[attrname] = defaults[attrname]
-            }
-        }
-        
-        else {
-            params = defaults;
-        }
 
     	var positive = print.getImageData(0,0, render.width, render.height)
         
-        if(params.blob){
-            var positive = params.blob           
+        if(blob){
+            var positive = blob           
         }
+        
         else {
         	for(var m = 0; m < render.width * render.height; m++){
         	    var index = m * 4;
@@ -95,35 +97,34 @@ module.exports = function(video, mirror, exposure, render){
                 positive.data[index + 3] = params.a
         	}   
         }
+    	var frame;
+    	
     	window.requestAnimationFrame(function(time){
     	    d = time + params.shutterSpeed
-    	    window.requestAnimationFrame(f)
+    	    frame = window.requestAnimationFrame(f)
     	})
     	
         function f(time){
-        	if(time > d) {
-                app.emit('expose', positive)
-        	    window.cancelAnimationFrame(f)
-        	    print.putImageData(positive, 0, 0)
-                console.log('fpc', fcp)
-    	        return
-    	    }
-        	window.requestAnimationFrame(f)
-        	fcp++
-        	var negative = reflection.getImageData(0,0,render.width,render.height);  
+            frame = window.requestAnimationFrame(f)
+            var negative = reflection.getImageData(0,0,render.width,render.height);  
             
             for(n=0; n<render.width*render.height; n++) {  
                 var index = n*4;   
-                var r = negative.data[index+0]
-                , g = negative.data[index+1]
-                , b = negative.data[index+2]
-                , re = (0.299 * r + 0.587 * g + 0.114 * b)
-                positive.data[index+0] =  vert(positive.data[index+0], (re / params.filmSpeed.r));
-                positive.data[index+1] = vert(positive.data[index+1], (re / params.filmSpeed.g));
-                positive.data[index+2] = vert(positive.data[index+2], (re / params.filmSpeed.b));
+                positive.data[index+0] =  vert(positive.data[index+0], negative.data[index+0] * params.filmSpeed.r);
+                positive.data[index+1] = vert(positive.data[index+1], negative.data[index+1] * params.filmSpeed.g);
+                positive.data[index+2] = vert(positive.data[index+2], negative.data[index+2] * params.filmSpeed.b);
                 positive.data[index + 3] = params.a;
             }
+                    	
+        	if(time > d) {
+        	    window.cancelAnimationFrame(frame)
 
+        	    print.putImageData(positive, 0, 0)
+        	    film.putImageData(positive, 0, 0)
+                app.emit('expose', positive)
+        	    
+    	        return
+    	    }
     	    film.putImageData(positive, 0, 0)
         };
         
